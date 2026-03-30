a984255dda37debf6f37ddf2597641cd1d8f24bb
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-31 00:30:22 +0300

    Create button.py

---
+++ ui/button.py
@@ -0,0 +1,25 @@
+""" The base class for buttons in the interface """
+import pygame
+
+
+class Button:
+    def __init__(self, x, y, width, height, text, color, hover_color):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.color = color
+        self.hover_color = hover_color
+        self.current_color = color
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            self.current_color = self.hover_color if self.rect.collidepoint(event.pos) else self.color
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                return True
+        return False
+
+    def draw(self, screen, font):
+        pygame.draw.rect(screen, self.current_color, self.rect)
+        text_surf = font.render(self.text, True, (0, 0, 0))
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        screen.blit(text_surf, text_rect)
