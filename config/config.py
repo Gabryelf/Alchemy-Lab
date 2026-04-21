b4d335d4091a7dbe9dac479f141032f65bcface0
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-04-21 18:17:36 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -10,7 +10,21 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (128, 128, 128)
 BLUE = (0, 100, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 # game states
 STATE_MENU = "menu"
 STATE_GAME = "game"
+
+enemy_types = {
+    1: "forward",
+    2: "warrior",
+    3: "tank"
+}
+
+images = {
+    1: "assets/images/enemy.png",
+    2: "assets/images/enemy2.png",
+    3: "assets/images/enemy.png"
+}
